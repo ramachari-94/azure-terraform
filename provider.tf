d04b1f2ee0725ed56d063ff00097722115dbc671
ramachari-94 <63164195+ramachari-94@users.noreply.github.com>
2022-12-14 13:00:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,4 +11,8 @@ terraform {
 provider "azurerm" {
   features {}
   skip_provider_registration = "true"
+  subscription_id   = "ab47269f-df82-4555-8cde-e85b89cead29"
+  tenant_id         = "e0386b10-5574-449a-8506-8749e0509e2c"
+  client_id         = "3a8fd1e9-4162-495f-a222-f3551a004df7"
+  client_secret     = "TGt8Q~5ybqW2Uhp9yR1FGyQR6_t8zM5RPTXAJcrC"
 }
